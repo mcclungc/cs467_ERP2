8aec037df6114c1dc0e8c9bfd8db4e712ff592ed
Terence Berry <berryte@flip3.engr.oregonstate.edu>
2019-04-13 17:54:33 -0700

    Website runs

--- db.js
+++ db.js
@@ -1,3 +1,11 @@
+/* 
+ * CS 467 Senior Project
+ * Authors: Terence Berry, Corey Immke, Connie McClung, Alexander Yfraimov
+ * File name: db.js
+ * Description: Database setup.
+ *
+*/
+
 var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
