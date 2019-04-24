270f9823d560870b043701e5692b940778e796e1
Terence Berry <berryte@flip3.engr.oregonstate.edu>
2019-04-23 21:22:32 -0700

    User Management Page Updates

--- public/js/admin.js
+++ public/js/admin.js
@@ -3,25 +3,21 @@
 */
 
 //Search function to display all results from database
+//Source: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_filter_table
 function search(){
-	var searchInput;
-	var filter;
-	var table;
-	var tr;
-	var td;
-	var itr;
+	var searchInput, filter, table, tr, td, itr;
 	searchInput = document.getElementById("myInput");
 	filter = searchInput.value.toUpperCase(); //make the search case insensitive
-	table = document.getElementByID("userTable");
+	table = document.getElementById("userTable");
 	tr = table.getElementsByTagName("tr");
-	for( i = 0; i < tr.length; i++){
-		td = tr[i].getElementsByTagName("td")[0];
+	for( itr = 0; itr < tr.length; itr++){
+		td = tr[itr].getElementsByTagName("td")[0];
 		if(td){
 			if(td.innerHTML.toUpperCase().indexOf(filter) > -1){
-				tr[i].style.display = "";
+				tr[itr].style.display = "";
 			}
 			else{
-				tr[i].style.display = "none";
+				tr[itr].style.display = "none";
 			}
 		}
 	}
