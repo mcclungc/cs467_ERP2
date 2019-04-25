58139862f87f10270b11908c3c4fe5eaf31931a5
Terence Berry <berryte@flip3.engr.oregonstate.edu>
2019-04-24 21:08:08 -0700

    Improved table functionality

--- public/js/admin.js
+++ public/js/admin.js
@@ -22,3 +22,89 @@ function search(){
 		}
 	}
 }
+
+/*Function for sorting the able rows from ascending to descending order*/
+function sortToggle(){
+	var col = this.parentNode.cellIndex;
+	if(ascending == true){
+		sortDescending(col);
+	}
+	else{
+		sortAscending(col);
+	}
+}
+
+function sortAscending(col) {
+	ascending = true;
+	var sorted = false;
+	var table, body, rows, td, itr;
+	table = document.getElementById("userTable");
+	body = document.getElementById("tableBody");
+	rows = table.getElementsByClassName("row");
+	while(!sorted){
+		for(itr = 0; itr < rows.length - 1; itr++){
+			if(rows[itr].cells[col].innerText.toUpperCase() > rows[itr + 1].cells[col].innerText.toUpperCase()){
+				body.insertBefore(rows[itr + 1], rows[itr]);
+			}
+		}
+		if(checkAscending(rows, col)){
+			sorted = true;
+		}
+		else{
+			sorted = false;
+		}
+	}
+}
+
+function checkAscending(array, col){
+	var itr;
+	for(itr = 0; itr < array.length - 1; itr++){
+		if(array[itr].cells[col].innerText.toUpperCase() > array[itr + 1].cells[col].innerText.toUpperCase()){
+			return false;
+		}
+	}
+	return true;
+}
+
+//Sory in descending order
+function sortDescending(col) {
+	ascending = false;
+	var sorted = false;
+	var table, body, rows, td, itr;
+	table = document.getElementById("userTable");
+	body = document.getElementById("tableBody");
+	rows = table.getElementsByClassName("row");
+	while(!sorted){
+		for(itr = 0; itr < rows.length - 1; itr++){
+			if(rows[itr].cells[col].innerText.toUpperCase() < rows[itr + 1].cells[col].innerText.toUpperCase()){
+				body.insertBefore(rows[itr + 1], rows[itr]);
+			}
+		}
+		if(checkDescending(rows, col)){
+			sorted = true;
+		}
+		else{
+			sorted = false;
+		}
+	}
+}
+
+//Check to see if we need to continue sorting
+function checkDescending(array, col){
+	var itr;
+	for(itr = 0; itr < array.length - 1; itr++){
+		if(array[itr].cells[col].innerText.toUpperCase() < array[itr + 1].cells[col].innerText.toUpperCase()){
+			return false;
+		}
+	}
+	return true;
+}
+
+var ascending = true;
+var toggle = document.getElementsByClassName("sort");
+var i;
+for(i = 0; i < toggle.length; i++){
+	toggle[i].addEventListener("click", sortToggle);
+}
+sortAscending(0);
+
