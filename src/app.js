3bbe33a2370c8aec404b5f554402272c0183cce3
Terence Berry <berryte@flip1.engr.oregonstate.edu>
2019-05-20 19:25:31 -0700

    Fixed table bug

--- src/app.js
+++ src/app.js
@@ -121,7 +121,7 @@ app.get('/admin-usermanagement', function(req, res, next){
 			var context = {};
 			context.layout = 'admin';
 			context.title = '| User Management';
-			mysql.pool.query('SELECT u.id as id, u.name as name, r.region_name as region_name, d.department_name as department_name, u.is_admin as userType, u.created_on as created_on FROM `users` u INNER JOIN `regions` r on u.region_id = r.id INNER JOIN `departments` d on u.department_id = d.id ORDER BY u.id', function(err, rows, fields) {
+			mysql.pool.query('SELECT u.id as id, u.name as name, u.email as email, r.region_name as region_name, d.department_name as department_name, u.is_admin as userType, u.created_on as created_on FROM `users` u INNER JOIN `regions` r on u.region_id = r.id INNER JOIN `departments` d on u.department_id = d.id ORDER BY u.id', function(err, rows, fields) {
 				if (err) {
 					next(err);
 					return;
